c48a3fe1a23512a5bd6c7bed992daadc20dbe184
RuparnaMukherjee <rupu3097@gmail.com>
2021-05-04 19:26:49 +0530

    structure

---
+++ src/main/java/com/cg/hims/entities/PolicyHolder.java
@@ -0,0 +1,18 @@
+package com.cg.hims.entities;
+
+public class PolicyHolder {
+
+	private int policyHolderId;
+	private String policyHolderName;
+	private String policyName;
+	private String premiumType;
+	private String creditCard;
+	private String dob;
+	private String occupation;
+	private double annualIncome;
+	private String isRetired;
+	private String ssn;
+	private String emailId;
+	private int policyId;
+	private HomeAddress address;
+}
