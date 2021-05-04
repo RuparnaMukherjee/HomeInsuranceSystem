12dc1482945e8108971c0427a0e333481598a4a3
RuparnaMukherjee <rupu3097@gmail.com>
2021-05-04 22:24:11 +0530

    admin n agent getter setter

--- src/main/java/com/cg/hims/entities/Admin.java
+++ src/main/java/com/cg/hims/entities/Admin.java
@@ -6,6 +6,29 @@ public class Admin {
 	private int adminId;
 	private String adminName;
 	private String contact;
+	public Admin(int adminId,String adminName,String contact) {
+		this.adminId=adminId;
+		this.adminName=adminName;
+		this.contact=contact;
+	}
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
 	
 	
 
--- src/main/java/com/cg/hims/entities/Agent.java
+++ src/main/java/com/cg/hims/entities/Agent.java
@@ -12,9 +12,73 @@ public class Agent {
 	private String address;
 	private String email;
 	private String mobileNo;
-	
 	private List<PolicyHolder> policyHoldersList;
 	private List<Policy> policies;
+	public Agent(int agentId,String agentName,String designation,String salary,String address,String email,String mobileNo,List<PolicyHolder> policyHoldersList,List<Policy> policies) {
+		this.agentId=agentId;
+		this.agentName=agentName;
+		this.designation=designation;
+		this.salary=salary;
+		this.address=address;
+		this.email=email;
+		this.mobileNo=mobileNo;
+		this.policyHoldersList=policyHoldersList;
+		this.policies=policies;
+	}
+	public int getAgentId() {
+		return agentId;
+	}
+	public void setAgentId(int agentId) {
+		this.agentId = agentId;
+	}
+	public String getAgentName() {
+		return agentName;
+	}
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public List<PolicyHolder> getPolicyHoldersList() {
+		return policyHoldersList;
+	}
+	public void setPolicyHoldersList(List<PolicyHolder> policyHoldersList) {
+		this.policyHoldersList = policyHoldersList;
+	}
+	public List<Policy> getPolicies() {
+		return policies;
+	}
+	public void setPolicies(List<Policy> policies) {
+		this.policies = policies;
+	}
 	
 
 }
